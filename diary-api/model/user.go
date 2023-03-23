929aeec432eaa94b677b5677f654a1058b632655
VicvekSr9485 <olamideoso65@gmail.com>
2023-03-23 09:33:29 +0100

    add authenticator and jwt generator

--- diary-api/model/user.go
+++ diary-api/model/user.go
@@ -15,7 +15,7 @@ type User struct {
     Entries  []Entry
 }
 
-func (user *User) save() (*User, error) {
+func (user *User) Save() (*User, error) {
     err := database.Database.Create(&user).Error
 
     if err != nil {
@@ -32,4 +32,17 @@ func (user *User) BeforeSave(*gorm.DB) error {
     user.Password = string(passwordHash)
     user.Username = html.EscapeString(strings.TrimSpace(user.Username))
     return nil
+}
+
+func (user *User) ValidatePassword(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+}
+
+func FindUserByUsername(username string) (User, error) {
+    var user User
+    err := database.Database.Where("username=?", username).Find(&user).Error
+    if err != nil {
+        return User{}, err
+    }
+    return user, nil
 }
