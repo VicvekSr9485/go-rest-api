6fc9518dd47f4c65e818dcdeba8cd0a7e12c6dac
VicvekSr9485 <olamideoso65@gmail.com>
2023-03-28 10:40:17 +0100

    add complete endpoints and entries

--- diary-api/controller/entry.go
+++ diary-api/controller/entry.go
@@ -1,35 +1,48 @@
 package controller
 
 import (
-	"diary-api/model"
 	"diary-api/helper"
-	"github.com/gin-gonic/gin"
+	"diary-api/model"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func AddEntry(context *gin.Context) {
-	var input model.entry
-	if err := context.ShouldBindJSON(&input), err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+	var input model.Entry
+	if err := context.ShouldBindJSON(&input); err != nil {
+
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := helper.CurrentUser(context)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	input.UserID = user.ID
 
 	savedEntry, err := input.Save()
+
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	context.JSON(http.StatusCreated, gin.H{"Data": savedEntry})
+
+	context.JSON(http.StatusCreated, gin.H{"data": savedEntry})
 }
 
 func GetAllEntries(context *gin.Context) {
-	user err := helper.CurrentUser(context)
+
+	user, err := helper.CurrentUser(context)
 
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	context.JSON(http.StatusOK, gin.H{"Entriess": user.Entries()})
+
+	context.JSON(http.StatusOK, gin.H{"data": user.Entries})
 }
